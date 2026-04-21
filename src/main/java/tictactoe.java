337ae11613c9bf2977bde3533726afa8c6dfc3de
gdruva220752 <target220.inmains@gmail.com>
2026-04-21 15:27:33 +0530

    UC2-TosstoDecideFirstPlayerandSymbol

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,19 +1,31 @@
-public class tictactoe {
-        public static void main(String[] args) {
-            char[][] board = new char[3][3];
+import java.util.Random;
 
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    board[i][j] = '-';
-                }
-            }
+class Toss {
+    public static void main(String[] args) {
+        Random random = new Random();
 
-            System.out.println("Tic-Tac-Toe Board:");
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(board[i][j] + " ");
-                }
-                System.out.println();
-            }
+        int tossResult = random.nextInt(2);
+
+        String player1 = "Player 1";
+        String player2 = "Player 2";
+
+        String currentPlayer;
+        char player1Symbol;
+        char player2Symbol;
+
+        if (tossResult == 0) {
+            currentPlayer = player1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = player2;
+            player2Symbol = 'X';
+            player1Symbol = 'O';
         }
+
+        System.out.println("Toss Result: " + (tossResult == 0 ? player1 : player2) + " starts");
+        System.out.println(player1 + " Symbol: " + player1Symbol);
+        System.out.println(player2 + " Symbol: " + player2Symbol);
+        System.out.println("Current Player: " + currentPlayer);
     }
+}
