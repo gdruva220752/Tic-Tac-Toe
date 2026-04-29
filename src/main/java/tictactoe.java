9a9152f7103fe0dfe433b2904fcbc0a15586ad8a
gdruva220752 <target220.inmains@gmail.com>
2026-04-29 14:55:41 +0530

    feature/UC7-computer-move

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,6 +1,9 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     private char[][] board = new char[3][3];
+    private Random rand = new Random();
 
     public TicTacToe() {
         initializeBoard();
@@ -14,58 +17,29 @@ public class TicTacToe {
         }
     }
 
-    public boolean updateBoard(int row, int col, char symbol) {
-        if (isValidMove(row, col)) {
-            board[row][col] = symbol;
-            return true;
-        } else {
-            System.out.println("Invalid move! Try again.");
-            return false;
-        }
-    }
-
     public boolean isValidMove(int row, int col) {
         return (row >= 0 && row < 3 &&
                 col >= 0 && col < 3 &&
                 board[row][col] == ' ');
     }
 
-    public void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
-            if (i < 2) System.out.println("---|---|---");
-        }
-    }
-}public class TicTacToe {
-
-    private char[][] board = new char[3][3];
-
-    public TicTacToe() {
-        initializeBoard();
+    public void updateBoard(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 
-    public void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = ' ';
+    public void computerMove(char symbol) {
+        int slot, row, col;
+
+        while (true) {
+            slot = rand.nextInt(9) + 1;
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+            if (isValidMove(row, col)) {
+                updateBoard(row, col, symbol);
+                break;
             }
         }
-    }
-
-    public boolean updateBoard(int row, int col, char symbol) {
-        if (isValidMove(row, col)) {
-            board[row][col] = symbol;
-            return true;
-        } else {
-            System.out.println("Invalid move! Try again.");
-            return false;
-        }
-    }
-
-    public boolean isValidMove(int row, int col) {
-        return (row >= 0 && row < 3 &&
-                col >= 0 && col < 3 &&
-                board[row][col] == ' ');
     }
 
     public void printBoard() {
